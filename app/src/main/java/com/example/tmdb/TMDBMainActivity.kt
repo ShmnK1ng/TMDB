0886b265fe6b6b9b5e2e99ec61d8a40abd8d9268
Andrei Shoka <xrdsby@gmail.com>
2023-05-11 17:25:55 +0300

    add Splash screen implementation

--- app/src/main/java/com/example/tmdb/TMDBMainActivity.kt
+++ app/src/main/java/com/example/tmdb/TMDBMainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.tmdb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class TMDBMainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tmdb_main)
     }
 }

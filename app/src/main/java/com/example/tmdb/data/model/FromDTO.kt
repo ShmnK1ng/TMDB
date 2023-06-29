15085a66a0990f56202ac19cf86fc05acf5a5af1
Andrei Shoka <xrdsby@gmail.com>
2023-06-29 12:30:05 +0300

    add refactor moviesDtoListToMovieList(), seriesDtoListToMovieList() functions

--- app/src/main/java/com/example/tmdb/data/model/FromDTO.kt
+++ app/src/main/java/com/example/tmdb/data/model/FromDTO.kt
@@ -1,25 +1,17 @@
 package com.example.tmdb.data.model
 
 fun List<MovieDto>.moviesDtoListToMovieList(): List<Movie> {
-    val movieList: MutableList<Movie> = mutableListOf()
-    this.map {
-        movieList.add(
-            Movie(
-                id = it.id, title = it.title, rating = it.rating, posterPath = it.posterPath
-            )
+    return this.map {
+        Movie(
+            id = it.id, title = it.title, rating = it.rating, posterPath = it.posterPath
         )
     }
-    return movieList.toList()
 }
 
 fun List<SeriesDto>.seriesDtoListToMovieList(): List<Movie> {
-    val movieList: MutableList<Movie> = mutableListOf()
-    this.map {
-        movieList.add(
-            Movie(
-                id = it.id, title = it.name, rating = it.rating, posterPath = it.posterPath
-            )
+    return this.map {
+        Movie(
+            id = it.id, title = it.name, rating = it.rating, posterPath = it.posterPath
         )
     }
-    return movieList.toList()
 }

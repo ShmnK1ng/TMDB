53404eea78e4c6980d617232f6f35b6c8cc174e2
Andrei Shoka <xrdsby@gmail.com>
2024-01-15 16:22:15 +0300

    refactor collectInViewModelScope function

--- app/src/test/java/com/example/tmdb/data/usecase/FakeGetCategoriesUseCase.kt
+++ app/src/test/java/com/example/tmdb/data/usecase/FakeGetCategoriesUseCase.kt
@@ -1,22 +1,13 @@
 package com.example.tmdb.data.usecase
 
-import com.example.tmdb.data.model.Category
-import com.example.tmdb.data.model.CategoryName
-import com.example.tmdb.data.model.Movie
 import com.example.tmdb.data.model.ResultCategories
-import com.example.tmdb.data.model.Type
-import com.example.tmdb.network.Result
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
 
-class FakeGetCategoriesUseCase : GetCategoriesUseCase {
+class FakeGetCategoriesUseCase(private val result: ResultCategories) : GetCategoriesUseCase {
     override fun getCategories(): Flow<ResultCategories> {
-        return MutableStateFlow(
-            ResultCategories(
-                listOf(
-                    Category(CategoryName(1, 1), listOf(Movie(0, "remoteId", "Title1", 0.0, "posterPath", Type.Series)))
-                ), Result.Failure.NetworkError
-            )
+        return flowOf(
+            result
         )
     }
 }
